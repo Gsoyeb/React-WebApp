2c4cadecf8acc22824879dca7a62ae625dde24bd
Gsoyeb <golamsoyeb@gmail.com>
2024-02-20 01:02:16 +0000

    issues

--- myproject/LiveJobs/views.py
+++ myproject/LiveJobs/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .LiveJobsRepo import LiveJobRepository
-
-
+from .LiveJobsSerializer import LiveJobSerializer
 
 def livejobs_list(request):
     livejobs = LiveJobRepository.get_all_live_jobs()
     return render(request, 'livejobs/testPage.html', {'livejobs': livejobs})
+
+class LiveJobsList(APIView):
+    def get(self, request):
+        livejobs = LiveJobRepository.get_all_live_jobs()
+        serializer = LiveJobSerializer(livejobs, many=True)  # 'many=True' for queryset
+        return Response(serializer.data)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -5,5 +5,6 @@ from LiveJobs import views as livejobsViews
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('livejobs/', livejobsViews.livejobs_list, name='livejobs_list'),
+    path('api/livejobs/', livejobsViews.LiveJobsList.as_view(), name='api-livejobs'),
 
 ]
